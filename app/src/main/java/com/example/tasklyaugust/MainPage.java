48cd1a99060e0d709ad1dc5b074096a9aac89e6e
TamUIC <tam.rice.cs@gmail.com>
2023-08-19 19:16:19 -0500

    8/19 Implement Nav Bar

--- app/src/main/java/com/example/tasklyaugust/MainPage.java
+++ app/src/main/java/com/example/tasklyaugust/MainPage.java
@@ -3,12 +3,42 @@ package com.example.tasklyaugust;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainPage extends AppCompatActivity {
+
+    private BottomNavigationView menuBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        menuBar = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+
+        menuBar.setOnItemSelectedListener( item -> {
+
+            if(item.getItemId() == R.id.home) {
+                Toast.makeText(this, item.getItemId() + " 1st button",  Toast.LENGTH_SHORT).show();
+            }
+            else if(item.getItemId() == R.id.calendar) {
+                Toast.makeText(this, item.getItemId() + " 2nd button",  Toast.LENGTH_SHORT).show();
+
+            }
+            else if(item.getItemId() == R.id.profile) {
+                Toast.makeText(this, item.getItemId() + " 3rd button",  Toast.LENGTH_SHORT).show();
+
+            }
+            else {
+                Toast.makeText(this, item.getItemId() + " 4th button",  Toast.LENGTH_SHORT).show();
+
+            }
+            return true;
+
+        });
     }
 }
